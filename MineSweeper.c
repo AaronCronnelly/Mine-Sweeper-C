38e38545a62328f00ef7e5532608557d0aaf7242
AaronCronnelly <G00403528@gmit.ie>
2022-12-29 14:02:42 +0000

    got the grid showing up, next is working on user input, to first get rid of the loaction, and then display how many mines are around that point

--- MineSweeper.c
+++ MineSweeper.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 
 //CONSTATNS
-  #define ROWS 10
-  #define COLS 10
+  #define ROWS 8
+  #define COLS 8
 
 //FUNCTIONS 
-  void MakeGrid(char Grid[ROWS][COLS]);
+  void MakeGrid(int Grid[ROWS][COLS]);
   void LoadGame();  
 
 int main()
@@ -15,22 +15,33 @@ int main()
 
 void LoadGame()
 {
-  char Grid[ROWS][COLS];
+  int Grid[ROWS][COLS];
   MakeGrid(Grid);
 }//END OF LOAD GAME
 
 
 
-void MakeGrid(char Grid[ROWS][COLS])
-{
-  for(int i=0; i<8; i++)
+void MakeGrid(int Grid[ROWS][COLS])
+{ 
+  for(int i=0; i<ROWS; i++)
     {
-      for(int j=0; j<8; j++)
+      for(int j=0; j<COLS; j++)
         {
-          Grid[i][j]='O';
+          //this is filling the main gird
+          Grid[i][j]=0;
+          //this is filling the top row of grid
+          Grid[0][j]=j+1;
+          //this is filling the first cols of grid 
+          Grid[i][0]=i+1;
+        }
+    }//MAKING GIRD
+  for(int i=0; i<ROWS; i++)
+    {
+      for(int j=0; j<COLS; j++)
+        {
+          printf(" %d ", Grid[i][j]);
         }
       printf("\n");
-    }
-
+    }//DISPLAYIG GIRD
 }//END OF MAKE GRID
 
